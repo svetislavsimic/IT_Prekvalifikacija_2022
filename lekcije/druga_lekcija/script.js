f43e6836c7a9a57d78adc93283721666ab40bfca
svetislav simic <svetislavsimic77@gmail.com>
2022-01-26 16:55:04 +0100

    addet third lesson

---
+++ lekcije/druga_lekcija/script.js
@@ -0,0 +1,34 @@
+//PROMENLJIVE U JS
+let starost = 44;
+const ime = "Svetislav";
+const zensko = false;
+const prezime = "Simic";
+const godinaRodjenja = 1977;
+const is_favorite_place = true;
+
+
+console.log(starost);
+/*
+Number
+String
+Boolean
+Symbol
+BigInt
+===
+undefined
+null
+===
+object
+*/
+
+//Operatori
+// +
+let trenutnaGodina = godinaRodjenja + starost;
+let punoIme = ime + prezime;
+// * mnozenje
+//* ** stepenovanje */
+// % ostatak pri delenju
+// vece manje jednako
+
+// alert
+//prompt
